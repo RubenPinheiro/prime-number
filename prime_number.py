dcd3554bc89daf2eea9cb04faa8ddd345becf572
RubenPinheiro <108191646+RubenPinheiro@users.noreply.github.com>
2022-06-25 23:26:29 +0100

    Update prime_number.py

--- prime_number.py
+++ prime_number.py
@@ -1,25 +1,20 @@
-#Write your code below this line 👇
 from math import sqrt
 
 def prime_checker(number):
 
-    prime_validator = 0
+    prime_validator = True
 
     if number == 1 or number == 2:
         print("It's a prime number.")
     
     for i in range (2, round(sqrt(number))):
         if number % i == 0:
-            prime_validator += 1
+            prime_validator = False
     
-    if prime_validator == 0:
+    if prime_validator:
         print("It's a prime number.")
     else:
         print("It's not a prime number.")
 
-
-#Write your code above this line 👆
-    
-#Do NOT change any of the code below👇
 n = int(input("Check this number: "))
 prime_checker(number=n)
